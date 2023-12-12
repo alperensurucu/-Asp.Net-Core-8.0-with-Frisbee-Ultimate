1a3a09da6c6818fe6bb72caa9765b381830e97d4
alper <alper@ALPEREN-PC>
2023-12-13 02:03:07 +0300

    Add project files.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using basics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var kurs = new Course();
+            kurs.Id = 1;
+            kurs.Title = "AspNet Core kursu";
+            kurs.Description = "Güzel bir kurs";
+
+            return View(kurs);
+        }
+        public IActionResult List()
+        {
+            var kurslar = new  List<Course>()
+            {
+                new Course(){ Id = 1, Title= "aspnet kursu", Description = "güzel bir kurs"},
+                new Course(){ Id = 2, Title= "php kursu", Description = "güzel bir kurs"},
+                new Course(){ Id = 3, Title= "django kursu", Description = "güzel bir kurs"}
+
+
+            };
+
+            return View("CourseList", kurslar);
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace basics.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
