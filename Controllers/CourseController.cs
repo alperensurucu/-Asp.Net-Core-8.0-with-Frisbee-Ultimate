a4c0556e7b2a3e823174cbdac2dc58fa4358c091
alper <alperensurucu.eng@gmail.com>
2023-12-26 19:45:17 +0300

    added İd?

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -15,6 +15,16 @@ namespace basics.Controllers
 
             return View(kurs);// Buraya yazılan kurs objesi , View altındaki index içinde karşılanmalı bu yüzden içine gidip, @model kurs yazılır.
         }
+        public IActionResult Details()
+        {
+            var kurs = new Course();
+            kurs.Id = 1;
+            kurs.Title = "Frizbee Ultimate";
+            kurs.Description = "Çok kural içeren bir takım oyunudur";
+            kurs.image = "1.jpg";
+
+            return View(kurs);
+        }
         public IActionResult List()
         {
             var kurslar = new  List<Course>()  // Şu anki modelimiz bir list . 
