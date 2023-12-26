e6dece42d3c8a6d247f33bfeac4b775cac7f83a9
alper <alperensurucu.eng@gmail.com>
2023-12-26 22:46:04 +0300

    added Repository for course lst

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -15,6 +15,7 @@ namespace basics.Controllers
 
             return View(kurs);// Buraya yazılan kurs objesi , View altındaki index içinde karşılanmalı bu yüzden içine gidip, @model kurs yazılır.
         }
+
         public IActionResult Details()
         {
             var kurs = new Course();
@@ -25,19 +26,10 @@ namespace basics.Controllers
 
             return View(kurs);
         }
+
         public IActionResult List()
         {
-            var kurslar = new  List<Course>()  // Şu anki modelimiz bir list . 
-            {
-                new Course(){ Id = 1, Title= "Frizbee Ultimate", Description = "7 ye 7 oynanan bir spor", image = "1.jpg"},
-                new Course(){ Id = 2, Title= "Bazen düşersin", Description = "yakalayamadı", image = "2.jpg"},
-                new Course(){ Id = 3, Title= "Sonra kalkarsın", Description = "yakalamak için hazır", image = "3.jpg"},
-                new Course(){ Id = 3, Title= "Takım arkadaşlarını yüreklendirirsin", Description = "pek işe yaramadı", image = "4.jpg"}
-
-
-            };
-
-            return View("CourseList", kurslar); // buradaki kursList  , View klasörü altında arayacağı dosya adı .eğer View () içi boş şekline ise bu şekilde o zaman metodun ismi olan List i arayacaktır.
+            return View("CourseList", Repository.Courses);  //Repository sınıfına alındı . Buradan course listesine ulaşıyoruz.
         }
     }
 }
---
+++ Models/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Models
+{
+    public class Repository 
+    {
+        private static readonly List<Course>  _courses = new();
+
+
+        static Repository()
+        {
+            _courses = new List<Course>()
+            {
+                new Course(){ Id = 1, Title= "Frizbee Ultimate", Description = "7 ye 7 oynanan bir spor", image = "1.jpg"},
+                new Course(){ Id = 2, Title= "Bazen düşersin", Description = "yakalayamadı", image = "2.jpg"},
+                new Course(){ Id = 3, Title= "Sonra kalkarsın", Description = "yakalamak için hazır", image = "3.jpg"},
+                new Course(){ Id = 3, Title= "Takım arkadaşlarını yüreklendirirsin", Description = "pek işe yaramadı", image = "4.jpg"},
+                new Course(){ Id = 3, Title= "Disk okuma", Description = "işe yaradı", image = "3.jpg"}
+
+            };
+
+        }
+        
+        public static List<Course> Courses 
+        {
+            get { return _courses; }
+        }
+    }
+}
